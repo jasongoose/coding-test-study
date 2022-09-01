200fd099e33d326309c26d9c9cf9da858169c389
신정규 <sjk@wemakeprice.com>
2022-09-01 15:56:18 +0900

    직접 만든 stack으로 문제 다시 풀어보기

--- 스택/같은_숫자는_싫어.js
+++ 스택/같은_숫자는_싫어.js
@@ -1,26 +1,52 @@
-function solution(arr) {
-  const answer = [arr[0]];
-  let top = 0;
-
-  for (let i = 1; i < arr.length; i++) {
-    if (answer[top] !== arr[i]) {
-      answer[++top] = arr[i];
-    }
-  }
-
-  return answer;
-}
-
-// 동일하게 동작하는데 왜 아래 풀이는 효율성 테스트에서 실패가 뜨는지 궁금합니다!
-// function solution([x, ...rest]) {
-//   const answer = [x];
+// function solution(arr) {
+//   const answer = [arr[0]];
 //   let top = 0;
 
-//   rest.forEach((r) => {
-//     if (answer[top] !== r) {
-//       answer[++top] = r;
+//   for (let i = 1; i < arr.length; i++) {
+//     if (answer[top] !== arr[i]) {
+//       answer[++top] = arr[i];
 //     }
-//   });
+//   }
 
 //   return answer;
 // }
+
+function myStack(size) {
+  this.status = Array(size).fill(null);
+  this.top = -1;
+
+  this.myPush = (val) => {
+    if (this.isFull()) throw Error("스택이 찼습니다.");
+    this.status[++this.top] = val;
+  };
+  this.myPop = () => {
+    if (this.isEmpty()) throw Error("스택이 비었습니다.");
+    this.status[this.top--] = null;
+  };
+  this.myPeek = () => {
+    if (this.isEmpty()) throw Error("스택이 비었습니다.");
+    return this.status[this.top];
+  };
+  this.isEmpty = () => this.top === -1;
+  this.isFull = () => this.top === size - 1;
+}
+
+const MAX = 1_000_000;
+
+function solution(arr) {
+  const ms = new myStack(MAX);
+  let ans = [];
+
+  ms.myPush(arr[0]);
+
+  for (let i=1; i<arr.length; i++) {
+    (ms.myPeek() !== arr[i]) && ms.myPush(arr[i]);
+  }
+  
+  while(!ms.isEmpty()) {
+    ans.push(ms.myPeek());
+    ms.myPop();
+  }
+
+  return ans.reverse();
+}
