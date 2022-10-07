b3c57810b6ab3ca365fc9a30dbaac887b41c1d08
신정규 <sjk@wemakeprice.com>
2022-10-07 17:16:18 +0900

    다리를 지나는 트럭 문제풀이

--- 큐/다리를_지나는_트럭.mjs
+++ 큐/다리를_지나는_트럭.mjs
@@ -1,32 +1,36 @@
 import { myQueue } from "./myQueue.mjs";
 
-const MAX = 10_000;
-
+// 참고자료
+// https://minhamina.tistory.com/241
 function solution(bridge_length, weight, truck_weights) {
-  const q = new myQueue(MAX);
-  let ans = 0;
-  let cnt = 0;
+  const q = new myQueue(bridge_length);
   let sum = 0;
-
+  let ans = 0;
+  
   for (let i = 0; i < truck_weights.length; i++) {
-    if (cnt < bridge_length && sum + truck_weights[i] <= weight) {
-      q.add(truck_weights[i]);
-      cnt++;
-      sum += truck_weights[i];
-    } else {
-      ans += bridge_length + cnt - 1;
-      while (!q.isEmpty()) {
+    while(true) {
+      if (q.isEmpty()) {
+        q.add(truck_weights[i]);
+        sum += truck_weights[i];
+        ans++;
+        break;
+      } else if (q.isFull()) {
+        sum -= q.peek();
         q.remove();
+      } else {
+        if (sum + truck_weights[i] <= weight) {
+          q.add(truck_weights[i]);
+          sum += truck_weights[i];
+          ans++;
+          break;
+        } else {
+          q.add(0);
+          ans++;
+        }
       }
-      cnt = 1;
-      q.add(truck_weights[i]);
-      sum = truck_weights[i];
     }
   }
-
-  if (!q.isEmpty()) {
-    ans += bridge_length + cnt - 1;
-  }
-
-  return ans + 1;
+  // 위 루프는 마지막 트럭이 다리에 오르자마자 끝나므로
+  // 마지막 트럭이 다리에서 내려오기까지 시간을 더하는 것으로 마무리 짓는다.
+  return ans + bridge_length;
 }
